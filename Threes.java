ec2d5d6d62a411de74aa63f9af47904951e94f85
yentrapati <143997541+yentrapati@users.noreply.github.com>
2023-09-04 12:41:49 +0530

    Create Threes.java

---
+++ Threes.java
@@ -0,0 +1,10 @@
+public int Threes(int x){
+
+    int i = 0;
+    for(int counter = 1; counter <= x; counter++){
+        if (counter % 3 ==0){
+            i ++;
+        }
+
+    }
+        return i;
